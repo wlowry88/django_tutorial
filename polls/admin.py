6db14fccbb1eee31a09d6123555c662839b1b05d
Will Lowry <william.c.lowry@gmail.com>
2014-09-26 16:27:58 -0400

    change new question form

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from polls.models import Poll
 
 class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
-
+    fieldsets = [
+            (None, {'fields' : ['question']}),
+            ('Date information', {'fields' : ['pub_date'], 'classes': ['collapse']}),
+    ]
 admin.site.register(Poll, PollAdmin)
