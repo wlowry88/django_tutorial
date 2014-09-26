a2eb2a27beaf113d7eec5bfbba17b2813e41407f
Will Lowry <william.c.lowry@gmail.com>
2014-09-26 16:38:45 -0400

    inline form fields

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from polls.models import Poll
-from polls.models import Choice
+from polls.models import Poll, Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
             (None, {'fields' : ['question']}),
             ('Date information', {'fields' : ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
