56702a6583389e8271074cac42815bb18f85a1f5
Will Lowry <william.c.lowry@gmail.com>
2014-09-26 16:58:41 -0400

    change default admin view

--- polls/admin.py
+++ polls/admin.py
@@ -11,5 +11,8 @@ class PollAdmin(admin.ModelAdmin):
             ('Date information', {'fields' : ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
 
 admin.site.register(Poll, PollAdmin)
