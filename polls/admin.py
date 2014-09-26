9e3aa4ecdc18b56ddaaa14dabbc64a117b0b0aff
Will Lowry <william.c.lowry@gmail.com>
2014-09-26 16:22:05 -0400

    change the admin polls admin file

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from polls.models import Poll
 
-# Register your models here.
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+admin.site.register(Poll, PollAdmin)
