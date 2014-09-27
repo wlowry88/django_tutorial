5eaa1ccf432eef2ed15e9dfb22609339b95982f5
Will Lowry <william.c.lowry@gmail.com>
2014-09-27 18:10:57 -0400

    switch to generic views fix urls and views

--- polls/views.py
+++ polls/views.py
@@ -1,33 +1,38 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-from polls.models import Poll
+from polls.models import Choice, Poll
 
-#def index(request):
-    #latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = RequestContext(request, {'latest_poll_list': latest_poll_list,
-    #})
-    #return HttpResponse(template.render(context))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
 
-def detail(request, poll_id):
-    #try:
-    #    poll = Poll.objects.get(pk=poll_id)
-    #except Poll.DoesNotExist:
-    #    raise Http404
-    #return render(request, 'polls/detail.html', {'poll': poll})
 
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
 
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try: 
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice bra.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
