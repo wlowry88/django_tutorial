db8e0e9eb25f5da28f258b09a94e345281a34781
Will Lowry <william.c.lowry@gmail.com>
2014-09-28 17:54:48 -0400

    test index view, passes tests

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 
+from django.utils import timezone
+
 from polls.models import Choice, Poll
 
 class IndexView(generic.ListView):
